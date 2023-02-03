fdc312b33d521ca55237b492eed6238247b11a60
Marco Exp <80280295+MarcoExpPer@users.noreply.github.com>
2023-02-03 20:11:13 +0100

    commit inicial

---
+++ Source/TheHook/TheHookGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheHookGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEHOOK_API ATheHookGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
