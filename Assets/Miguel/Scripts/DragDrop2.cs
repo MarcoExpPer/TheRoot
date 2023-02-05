a5f41f9fe8d536b60d417859d769a3049008192b
popletox <57769623+popletox@users.noreply.github.com>
2023-02-05 09:58:25 +0100

    Clamp DragDrop

--- Assets/Miguel/Scripts/DragDrop2.cs
+++ Assets/Miguel/Scripts/DragDrop2.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class DragDrop2 : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [HideInInspector] public RectTransform rt;
     private RectTransform parent_rt;
-    private Vector2 middlePoint = new Vector2(547.5f, 310f);
+    private Vector2 middlePoint = new Vector2(0f, 0f);
 
     private void Start()
     {
@@ -22,11 +22,11 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 
     }
 
-    public void OnDrag(PointerEventData EventData) 
+    public void OnDrag(PointerEventData EventData)
     {
-        if (rt.anchoredPosition.x > 0f + rt.sizeDelta.x / 2 && rt.anchoredPosition.x < 1090f - rt.sizeDelta.x / 2 &&
-            rt.anchoredPosition.y > 0f + rt.sizeDelta.y / 2 && rt.anchoredPosition.y < 620f - rt.sizeDelta.y / 2)
-        { 
+        if (rt.anchoredPosition.x > -550f + rt.sizeDelta.x / 2 && rt.anchoredPosition.x < 550f - rt.sizeDelta.x / 2 &&
+            rt.anchoredPosition.y > -380f + rt.sizeDelta.y / 2 && rt.anchoredPosition.y < 240f - rt.sizeDelta.y / 2)
+        {
             rt.anchoredPosition += EventData.delta;
             transform.SetAsLastSibling();
         }
@@ -36,11 +36,11 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
             float despY = ((rt.anchoredPosition.y - middlePoint.y) <= 0f) ? 1f : -1f;
             rt.anchoredPosition += new Vector2(despX, despY);
         }
-        
+
     }
 
-    public void OnEndDrag(PointerEventData EventData) 
-    { 
+    public void OnEndDrag(PointerEventData EventData)
+    {
 
     }
 
