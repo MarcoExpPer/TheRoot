a637dd5a8d10b08d41dda7c65e5616bff3ad68fa
Marco Exp <80280295+MarcoExpPer@users.noreply.github.com>
2023-02-06 15:23:51 +0100

    a

--- Assets/Miguel/Scripts/Window.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Window : MonoBehaviour
-{
-    [HideInInspector] public bool minimized = false;
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# TheRoot
-
-Si tienen grupo de Discord o algo, agregarme. Mi Discord es AlexXW#8226
